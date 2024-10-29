4c4c1d5e7d6a2a4646bbe6443ddafcc50ff40e9f
Stanislavsky <91017544+Stanislavsky@users.noreply.github.com>
2024-10-29 14:42:29 +0300

    Lab1. Task 3. Avoid duplication of code

--- Student.kt
+++ Student.kt
@@ -1,71 +1,30 @@
-class Student(
+data class Student(
     private var id: Int,
     private var lastName: String,
     private var firstName: String,
-    private var middleName: String,
-    private var phone: String? = null,
-    private var telegram: String? = null,
     private var email: String? = null,
     private var gitHub: String? = null
-) {
-    fun getId(): Int {
-        return id
-    }
-    fun getLastName(): String {
-        return lastName
-    }
-    fun getFirstName(): String {
-        return firstName
-    }
-    fun getMiddleName(): String? {
-        return middleName
-    }
-    fun getPhone(): String? {
-        return phone
-    }
-    fun getTelegram(): String? {
-        return telegram
-    }
-    fun getEmail(): String? {
-        return email
-    }
-    fun getGitHub(): String? {
-        return gitHub
-    }
-    fun setId(newId: Int) {
-        id = newId
-    }
-    fun setLastName(newLastName: String) {
-        lastName = newLastName
-    }
-    fun setFirstName(newFirstName: String) {
-        firstName = newFirstName
-    }
-    fun setMiddleName(newMiddleName: String) {
-        middleName = newMiddleName
-    }
-    fun setPhone(newPhone: String?) {
-        phone = newPhone
-    }
-    fun setTelegram(newTelegram: String?) {
-        telegram = newTelegram
-    }
-    fun setEmail(newEmail: String?) {
-        email = newEmail
-    }
-    fun setGitHub(newGitHub: String?) {
-        gitHub = newGitHub
-    }
-    fun showInfo(): String {
+)
+{
+    override fun toString(): String
+    {
         return """
             ID: $id
             Фамилия: $lastName
             Имя: $firstName
-            Отчество: ${middleName}
+            Отчество: ${middleName ?: "Не указано"}
             Телефон: ${phone ?: "Не указан"}
             Телеграм: ${telegram ?: "Не указан"}
             Почта: ${email ?: "Не указана"}
             Гит: ${gitHub ?: "Не указан"}
         """.trimIndent()
     }
+
+    override fun equals(other: Any?): Boolean
+    {
+        if (this === other) return true
+        if (other !is Student) return false
+        return id == other.id
+    }
+
 }
