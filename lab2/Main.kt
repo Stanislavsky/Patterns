b46fc7a69a9881a59967f592ea691e0bfbff3f21
Stanislavsky <12twelve12kek@mail.ru>
2024-12-10 16:47:07 +0300

    LR 2.4 - Добавил класс StudentShort

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 import Student
+import StudentShort
 
 fun main() {
 //    val students = mutableListOf<Student>();
@@ -18,9 +19,14 @@ fun main() {
 //    stud.setContacts(hashMapOf(Pair("email","literature@gmail.com"),Pair("telegram","@miha999"),Pair("gitHub",null)));
 //    println(stud)
 //    stud.name = "Мишаня";
-    println(stud)
-    val data = Student.parseString("Student(id:1,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
+
+    val data = Student.parseString("Student(id:4,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
     println(data)
+    println(stud)
     println(Student(data))
+    println(Student(Student.parseString("Student(id:1,surname:Romanov,name:Vladislav,patronymic:Vitalevich,phoneNumber:+78005553535,email:romadis@gmail.com,telegram:,gitHub:romadis)")))
     println(stud.getInfo())
+    println(StudentShort(4,stud.getInfo()))
+    println(StudentShort(stud))
 }
+
