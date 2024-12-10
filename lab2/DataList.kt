4ad6ec99226fdf1be832d32990de132c9e8c283e
Stanislavsky <12twelve12kek@mail.ru>
2024-12-10 20:10:11 +0300

    LR 2.16-2.18 - Отрефакторил код

--- lab2/DataList.kt
+++ lab2/DataList.kt
@@ -1,6 +1,7 @@
-abstract class DataList<T>(val elements: Array<T>) {
-    private val selectedIndices = mutableSetOf<Int>()
+import kotlin.reflect.full.memberProperties
 
+open class DataList<T>(val elements: Array<T>) {
+    private val selectedIndices = mutableSetOf<Int>()
     // Выделение элемента по номеру
     fun select(number: Int) {
         if (number in elements.indices) {
@@ -14,8 +15,20 @@ abstract class DataList<T>(val elements: Array<T>) {
     fun getSelectedIds() = selectedIndices.toIntArray()
 
     // Получение массива наименований атрибутов
-    abstract fun getNames(): Array<String>
+    open fun functionGetPropsNames() = if(this.elements.isEmpty()) arrayOf() else this.elements[0]!!::class.memberProperties.map{it.name}.toTypedArray<String>()
+    fun getNames(): Array<String> = functionGetPropsNames()
 
     // Получение DataTable
-    abstract fun getData(): DataTable
+    open fun getPropertiesOfClass(value:T):List<Any?> = arrayOf("12",2).toList()
+    fun getData(): DataTable {
+        val dataList:MutableList<Array<Any?>> = mutableListOf()
+        var rowNumber = 0;
+        for(el in this.elements){
+            dataList.add(
+                arrayOf(rowNumber,*getPropertiesOfClass(el).toTypedArray<Any?>())
+            )
+            rowNumber+=1;
+        }
+        return DataTable(dataList.toTypedArray<Array<Any?>>())
+    }
 }
