793602dc6d82c8f591218c933a1514dd956ae92b
Stanislavsky <12twelve12kek@mail.ru>
2024-12-10 16:52:56 +0300

    LR 2.5-2.6 - Отрефакторил код и построил диаграмму классов

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -4,29 +4,29 @@ import Student
 import StudentShort
 
 fun main() {
-//    val students = mutableListOf<Student>();
-//    students.add(Student("Романов","Владислав","Витальевич"));
-//    students.add(Student("Мамин-Сибиряк","Дмитрий","Наркисович", telegramValue = "@poet777"));
-//    students.add(Student("Махмудов","Арарат-Магомед","Саркисович", gitHubValue = "abu123"));
-//    students.add(Student("Лермонтов","Михаил","Юрьевич", emailValue = "literature@gmail.com"));
-//    students.add(Student("Баладин","Максим","Викторович", phoneNumberValue = "+79095889812"));
-//    students.add(Student(hashMapOf(Pair("name","Владислав"),Pair("surname","Романов"),Pair("patronymic","Витальевич"))));
-//    students.forEach { it: Student -> println(it) };
-//    println(Student(hashMapOf(Pair("name","Владислав"),Pair("surname","Романов"),Pair("patronymic","Витальевич"))).validate())
-//    println(Student("Лермонтов","Михаил","Юрьевич", emailValue = "literature@gmail.com", gitHubValue = "famous007").validate());
+    val students = mutableListOf<Student>();
+    students.add(Student("Romanov","Vladislav","Vitalevich"));
+    students.add(Student("Mamin-Sibiryak","Dmitriy","Narkisovich", telegramValue = "@poet777"));
+    students.add(Student("Mahmudov","Ararat","Sarkisovich", gitHubValue = "abu123"));
+    students.add(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com"));
+    students.add(Student("Chernikov","Alexander","Viktorovich", phoneNumberValue = "+79095889812"));
+    students.add(Student(hashMapOf(Pair("name","Vladislav"),Pair("surname","Romanov"),Pair("patronymic","Vitalevich"))));
+    students.forEach { it: Student -> println(it) };
+    println(Student(hashMapOf(Pair("name","Vladislav"),Pair("surname","Romanov"),Pair("patronymic","Vitalevich"))).validate())
+    println(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007").validate());
 
     val stud = Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007", phoneNumberValue = "+79186916942");
-//    stud.setContacts(hashMapOf(Pair("email","literature@gmail.com"),Pair("telegram","@miha999"),Pair("gitHub",null)));
-//    println(stud)
-//    stud.name = "Мишаня";
+    stud.setContacts(hashMapOf(Pair("email","literature@gmail.com"),Pair("telegram","@miha999"),Pair("gitHub",null)));
+    println(stud)
+    stud.name = "Mishanya";
 
     val data = Student.parseString("Student(id:4,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
     println(data)
     println(stud)
     println(Student(data))
-    println(Student(Student.parseString("Student(id:1,surname:Romanov,name:Vladislav,patronymic:Vitalevich,phoneNumber:+78005553535,email:romadis@gmail.com,telegram:,gitHub:romadis)")))
+    println(Student(Student.parseString("Student(id:3,surname:Chernenko,name:Viktor,patronymic:Grigorevich,gitHub:)")))
     println(stud.getInfo())
-    println(StudentShort(4,stud.getInfo()))
+    println(StudentShort(4,Student("Lermontov","Mihail","Yurevich", gitHubValue = "famous007", telegramValue = "@poetishka").getInfo()))
     println(StudentShort(stud))
 }
 
